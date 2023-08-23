0b216f1ba83f829adf0a5ad5e47ac6e53c59aabf
hassan <hassan@thiqatech.com>
2023-08-23 23:16:08 +0400

    added admin

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
-    <title>{{ __("admin.admin_panel") }} - {{ config('app.name') }}</title>
+    <title>Espacio de Administración del Sistema de #PilandoAndo</title>
 
     <meta name="author" content="pixelcave">
     <meta name="robots" content="noindex, nofollow">
